9ee5b9802c978527ccaf6b60314176448d773d24
Nihat <nihat0851@gmail.com>
2023-09-11 02:31:36 +0300

    project is finished.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
-import { ExampleComponent } from 'button-types-ui'
-import 'button-types-ui/dist/index.css'
+import { Button } from 'button-ui'
+import 'button-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='container'>
+      <div className='center-content'>
+        <Button text='Text Button' type='text' />
+        <br />
+        <br />
+        <Button text='Default Button ' type='default' />
+        <br />
+        <br />
+        <Button text='Dashed Button' type='dashed' />
+        <br />
+        <br />
+        <Button text='Primary Button' type='primary' />
+        <br />
+        <br />
+        <Button text='Link Button' type='link' />
+      </div>
+    </div>
+  )
 }
 
 export default App
